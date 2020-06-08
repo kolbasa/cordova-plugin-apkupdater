d935b490c91425891f274e47ce370d012d08f956
Michael Jedich <m.jedich@gmail.com>
2020-06-08 20:51:18 +0200

    API Promise-Support #1

---
+++ www/API.js
@@ -0,0 +1,74 @@
+var exec = require('cordova/exec');
+
+var MODULE_NAME = 'ApkUpdater';
+
+var _observer;
+
+module.exports = {
+
+    check: function (manifestUrl) {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'check', [manifestUrl]);
+        });
+    },
+
+    download: function () {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'download', []);
+        });
+    },
+
+    backgroundDownload: function (interval) {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'backgroundDownload', [interval]);
+        });
+    },
+
+    stop: function () {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'stop', []);
+        });
+    },
+
+    install: function () {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'install', []);
+        });
+    },
+
+    reset: function () {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, MODULE_NAME, 'reset', []);
+        });
+    },
+
+    setObserver: function (observer) {
+        _observer = observer;
+    },
+
+    onDownloadProgress: function (percentage, bytes, bytesDownloaded, chunks, chunksDownloaded) {
+        if (_observer != null && _observer.downloadProgress != null) {
+            _observer.downloadProgress(percentage, bytes, bytesDownloaded, chunks, chunksDownloaded);
+        }
+    },
+
+    onUnzipProgress: function (percentage, bytes, bytesDownloaded) {
+        if (_observer != null && _observer.unzipProgress != null) {
+            _observer.unzipProgress(percentage, bytes, bytesDownloaded);
+        }
+    },
+
+    onException: function (exception, details) {
+        if (_observer != null && _observer.exception != null) {
+            _observer.exception(exception, details);
+        }
+    },
+
+    onEvent: function (event) {
+        if (_observer != null && _observer.event != null) {
+            _observer.event(event);
+        }
+    }
+
+}
+
---
+++ www/ApkUpdater.js
@@ -0,0 +1,117 @@
+var API = require('./API');
+
+module.exports = {
+
+    /**
+     * @param {string} manifestUrl - The url to your hosted manifest file
+     * @param {function=} success - Returns the parsed manifest object
+     * @param {function=} failure
+     *
+     * @returns {Promise<object>|void}
+     */
+    check: function (manifestUrl, success, failure) {
+        if (success == null && failure == null) {
+            return API.check(manifestUrl);
+        } else {
+            API.check(manifestUrl).then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {function=} success
+     * @param {function=} failure
+     *
+     * @returns {Promise<void>|void}
+     */
+    download: function (success, failure) {
+        if (success == null && failure == null) {
+            return API.download();
+        } else {
+            API.download().then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {number} interval - How fast individual parts should be downloaded in milliseconds.
+     * @param {function=} success
+     * @param {function=} failure
+     *
+     * @returns {Promise<void>|void}
+     */
+    backgroundDownload: function (interval, success, failure) {
+        if (success == null && failure == null) {
+            return API.backgroundDownload(interval);
+        } else {
+            API.backgroundDownload(interval).then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {function=} success
+     * @param {function=} failure
+     *
+     * @returns {Promise<void>|void}
+     */
+    stop: function (success, failure) {
+        if (success == null && failure == null) {
+            return API.stop();
+        } else {
+            API.stop().then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {function=} success
+     * @param {function=} failure
+     *
+     * @returns {Promise<void>|void}
+     */
+    install: function (success, failure) {
+        if (success == null && failure == null) {
+            return API.install();
+        } else {
+            API.install().then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {function=} success
+     * @param {function=} failure
+     *
+     * @returns {Promise<void>|void}
+     */
+    reset: function (success, failure) {
+        if (success == null && failure == null) {
+            return API.reset();
+        } else {
+            API.reset().then(success).catch(failure);
+        }
+    },
+
+    /**
+     * @param {object} observer
+     * @returns {void}
+     */
+    setObserver: API.setObserver,
+
+    /**
+     * @private
+     */
+    _downloadProgress: API.onDownloadProgress,
+
+    /**
+     * @private
+     */
+    _unzipProgress: API.onUnzipProgress,
+
+    /**
+     * @private
+     */
+    _exception: API.onException,
+
+    /**
+     * @private
+     */
+    _event: API.onEvent
+
+};
