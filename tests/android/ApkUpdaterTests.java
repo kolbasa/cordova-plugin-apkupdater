fcb7863371356f9d6a954e9435077be97805b679
Michael Jedich <michael.jedich@pm.me>
2020-06-20 18:22:54 +0200

    Work on the test coverage.

--- tests/android/ApkUpdaterTests.java
+++ tests/android/ApkUpdaterTests.java
@@ -162,20 +162,17 @@ class ApkUpdaterTests {
     }
 
     private int waitForFile(String part) throws InterruptedException {
-        return waitForFile(part, MAX_DOWNLOAD_TIME);
-    }
-
-    private int waitForFile(String part, Integer waitTime) throws InterruptedException {
-        for (int i = 0; i < waitTime / 5; i++) {
+        for (int i = 0; i < MAX_DOWNLOAD_TIME / 5; i++) {
             Thread.sleep(5);
             File chunk = new File(updateDirectory, part);
             File lock = new File(updateDirectory, part + ".lock");
 
             if (chunk.exists() && chunk.length() > 0 && !lock.exists()) {
-                return i * 5;
+                Thread.sleep(25);
+                return i * 5 + 25;
             }
         }
-        return waitTime;
+        return MAX_DOWNLOAD_TIME;
     }
 
     private void waitForException(ArrayList<Exception> list) throws InterruptedException {
@@ -188,6 +185,15 @@ class ApkUpdaterTests {
                 return;
             }
         }
+    }
+
+    private DownloadProgress findDownloadProgressEvent(ArrayList<DownloadProgress> events, int part) {
+        for (int i = 0; i < events.size(); i++) {
+            if (events.get(i).getChunksDownloaded() == part) {
+                return events.get(i);
+            }
+        }
+        return null;
     }
 
     private String downloadDirectory;
@@ -380,37 +386,32 @@ class ApkUpdaterTests {
 
                 Thread.sleep(DOWNLOAD_INTERVAL_IN_MS - shift);
                 shift = waitForFile(PART_01);
-                assertEquals(2, events.size());
+                assertTrue(events.size() >= 2);
 
-                DownloadProgress event;
+                DownloadProgress progress;
 
-                // there is one event at the start of the download
-                event = events.get(0);
-                assertEquals(0, event.getBytesWritten());
-                assertEquals(0, event.getChunksDownloaded());
+                progress = events.get(0);
+                assertEquals(0, progress.getChunksDownloaded());
+                assertEquals(0, progress.getPercent());
 
-                event = events.get(1);
-                assertEquals(1, event.getChunksDownloaded());
-                assertEquals(150, event.getBytesWritten());
-                assertEquals(42.74f, event.getPercent());
+                progress = findDownloadProgressEvent(events, 1);
+                assertNotNull(progress);
+                assertEquals(42.74f, progress.getPercent());
+
 
                 Thread.sleep(DOWNLOAD_INTERVAL_IN_MS - shift);
                 shift = waitForFile(PART_02);
 
-                assertEquals(3, events.size());
-                event = events.get(2);
-                assertEquals(2, event.getChunksDownloaded());
-                assertEquals(300, event.getBytesWritten());
-                assertEquals(85.47f, event.getPercent());
+                progress = findDownloadProgressEvent(events, 2);
+                assertNotNull(progress);
+                assertEquals(85.47f, progress.getPercent());
 
                 Thread.sleep(DOWNLOAD_INTERVAL_IN_MS - shift);
                 waitForFile(PART_03);
 
-                assertEquals(4, events.size());
-                event = events.get(3);
-                assertEquals(3, event.getChunksDownloaded());
-                assertEquals(event.getBytes(), event.getBytesWritten());
-                assertEquals(100f, event.getPercent());
+                progress = events.get(events.size() - 1);
+                assertEquals(100, progress.getPercent());
+                assertEquals(3, progress.getChunksDownloaded());
             }
 
         }
@@ -660,22 +661,50 @@ class ApkUpdaterTests {
             void shouldSkipAlreadyDownloaded() throws Exception {
                 // noinspection ResultOfMethodCallIgnored
                 new File(updateDirectory).mkdir();
-                copyFile(new File(LOCAL + UPDATE, PART_01), new File(updateDirectory, PART_01));
+                File part = new File(updateDirectory, PART_01);
+                copyFile(new File(LOCAL + UPDATE, PART_01), part);
 
                 UpdateManager updater = new UpdateManager(REMOTE_UPDATE, downloadDirectory);
                 updater.check();
 
                 ArrayList<DownloadProgress> events = observe(updater).getDownloadProgress();
 
+                part = new File(updateDirectory, PART_01);
+                assertEquals(ChecksumGenerator.getFileChecksum(part), PART_01_MD5_HASH);
+
+                part = new File(updateDirectory, PART_02);
+                assertFalse(part.exists());
+
+                part = new File(updateDirectory, PART_03);
+                assertFalse(part.exists());
+
                 updater.download();
 
-                assertEquals(3, events.size());
-                assertEquals(3, events.get(0).getChunks());
+                part = new File(updateDirectory, PART_01);
+                assertEquals(ChecksumGenerator.getFileChecksum(part), PART_01_MD5_HASH);
 
-                // TODO: Better check file-hash
-                assertEquals(0, events.get(0).getChunksDownloaded());
-                assertEquals(2, events.get(1).getChunksDownloaded());
-                assertEquals(3, events.get(2).getChunksDownloaded());
+                part = new File(updateDirectory, PART_02);
+                assertEquals(ChecksumGenerator.getFileChecksum(part), PART_02_MD5_HASH);
+
+                part = new File(updateDirectory, PART_03);
+                assertEquals(ChecksumGenerator.getFileChecksum(part), PART_03_MD5_HASH);
+
+                DownloadProgress progress;
+                // 2 downloaded chunk events + 1 start event
+                // plus x intermediate progress events
+                assertTrue(events.size() >= 3);
+
+                progress = events.get(0);
+                assertEquals(1, progress.getChunksDownloaded());
+                assertEquals(42.74f, progress.getPercent());
+
+                progress = findDownloadProgressEvent(events, 2);
+                assertNotNull(progress);
+                assertEquals(85.47f, progress.getPercent());
+
+                progress = events.get(events.size() - 1);
+                assertEquals(100, progress.getPercent());
+                assertEquals(3, progress.getChunksDownloaded());
             }
         }
 
@@ -734,9 +763,21 @@ class ApkUpdaterTests {
 
                 ArrayList<UnzipProgress> events = observe(updater).getUnzipProgress();
                 updater.download();
-                assertEquals(2, events.size());
-                assertEquals(0, events.get(0).getPercent());
-                assertEquals(100f, events.get(1).getPercent());
+
+                UnzipProgress progress;
+                // 1 start event + 1 end event + 1 progress event
+                // plus x intermediate progress events
+                assertTrue(events.size() >= 3);
+
+                progress = events.get(0);
+                assertEquals(0, progress.getPercent());
+
+                progress = events.get(1);
+                assertTrue(progress.getPercent() > 0);
+                assertTrue(progress.getPercent() < 100);
+
+                progress = events.get(events.size() - 1);
+                assertEquals(100, progress.getPercent());
             }
 
             @Test
@@ -747,11 +788,27 @@ class ApkUpdaterTests {
 
                 ArrayList<DownloadProgress> events = observe(updater).getDownloadProgress();
                 updater.download();
-                assertEquals(4, events.size());
-                assertEquals(0, events.get(0).getPercent());
-                assertEquals(42.74f, events.get(1).getPercent());
-                assertEquals(85.47f, events.get(2).getPercent());
-                assertEquals(100f, events.get(3).getPercent());
+
+                DownloadProgress progress;
+                // 3 downloaded chunk events + 1 start event
+                // plus x intermediate progress events
+                assertTrue(events.size() >= 4);
+
+                progress = events.get(0);
+                assertEquals(0, progress.getChunksDownloaded());
+                assertEquals(0, progress.getPercent());
+
+                progress = findDownloadProgressEvent(events, 1);
+                assertNotNull(progress);
+                assertEquals(42.74f, progress.getPercent());
+
+                progress = findDownloadProgressEvent(events, 2);
+                assertNotNull(progress);
+                assertEquals(85.47f, progress.getPercent());
+
+                progress = events.get(events.size() - 1);
+                assertEquals(100, progress.getPercent());
+                assertEquals(3, progress.getChunksDownloaded());
             }
 
             @Test
